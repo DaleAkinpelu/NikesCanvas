ea9d0f871fc37349ee0426a044ca56cbabf12d83
Boladale Akinpelu <83141300+DaleAkinpelu@users.noreply.github.com>
2022-07-13 00:58:58 -0400

    Last Update before launch
    Fixed a bunch of bugs like:
    -The footer not being at the bottom
    -Optimized it for mobile

--- Style.js
+++ Style.js
@@ -11,3 +11,35 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function() {
     myInput.focus()
 })
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+var demo1 = document.getElementById("demo1");
+var demo2 = document.getElementById("demo2");
+var demo3 = document.getElementById("demo3");
+
+
+while (window.innerWidth <= 575.98) {
+    demo1.classList.remove("btn-lg");
+    demo2.classList.remove("btn-lg");
+    demo3.classList.remove("btn-lg");
+
+    demo1.classList.add("btn-sm");
+    demo2.classList.add("btn-sm");
+    demo3.classList.add("btn-sm");
+
+}
